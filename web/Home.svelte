189fd25efc00d4dafb4acefda5a45368cbff036b
Randy <randypriv@gmail.com>
2021-02-27 14:22:30 +0800

    change block event

--- web/Home.svelte
+++ web/Home.svelte
@@ -1,32 +1,20 @@
 <script>
-  import Block from "plastic-editor";
+  import Page from './Page.svelte'
 
-  let block = {
-    id: "root",
-    content: "",
+
+  let page = {
+    id: 'foo',
+    title: 'Tutorial',
     children: [
-      {
-        id: "empty",
-        content: "",
-        children: [],
-      },
-    ],
-  };
+
+    ]
+  }
 </script>
 
 <div class="flex">
   <div class="flex-1">
     <div style="width: 960px;" class="mx-auto">
-      <div class="px-8 pt-16 mb-8">
-        <input
-          value="Test page"
-          class="outline-none text-2xl font-bold w-full"
-          placeholder="Page title"
-        />
-      </div>
-      <div class="px-8">
-        <Block {block} root={block} path={[0]} />
-      </div>
+      <Page page={page} />
     </div>
   </div>
 </div>
---
+++ web/Page.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Block from "plastic-editor";
+  export let page;
+
+  let block = {
+    id: "root",
+    content: "",
+    children: page.children.length ? page.children : [
+      {
+        id: `${Date.now()}`,
+        content: '',
+        children: []
+      }
+    ],
+  };
+
+  function onChangeBlock() {
+    console.log('update', block)
+  }
+</script>
+
+<div class="px-8 pt-16 mb-8">
+  <input
+    value={page.title}
+    class="outline-none text-2xl font-bold w-full"
+    placeholder="Page title"
+  />
+</div>
+<div class="px-8">
+  <Block dispatchChangeBlockToRoot={onChangeBlock} {block} root={block} path={[0]} />
+</div>
